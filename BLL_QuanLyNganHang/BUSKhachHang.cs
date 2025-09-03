73a9cca5d47ec2428a1814f8fc3e761acebe6bec
Colek123 <colek.2k@gmail.com>
2025-09-03 16:19:19 +0700

    up code BLL again

---
+++ BLL_QuanLyNganHang/BUSKhachHang.cs
@@ -0,0 +1,80 @@
+﻿using DAL_QuanLyNganHang;
+using DTO_QuanLyNganHang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_QuanLyNganHang
+{
+    public class BUSKhachHang
+    {
+        DALKhachHang dalKhachHang = new DALKhachHang();
+
+        public List<KhachHang> GetKhachHangList(int trangThai = -1)
+        {
+            return dalKhachHang.selectAll(trangThai);
+        }
+        public string InsertKhachHang(KhachHang kh)
+        {
+            try
+            {
+                kh.MaKH = dalKhachHang.generateMaKhachHang();
+                if (string.IsNullOrEmpty(kh.MaKH))
+                {
+                    return "Mã khách hàng không hợp lệ.";
+                }
+
+                dalKhachHang.insertKhachHang(kh);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Thêm mới không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        public string UpdateKhachHang(KhachHang kh)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(kh.MaKH))
+                {
+                    return "Mã khách hàngm không hợp lệ.";
+                }
+
+                dalKhachHang.updateKhachHang(kh);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Cập nhật không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        public string DeleteKhachHang(string maKH)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(maKH))
+                {
+                    return "Mã khách hàng không hợp lệ.";
+                }
+
+                dalKhachHang.deleteKhachHang(maKH);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Xóa không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        public List<string> GetAllMaKH()
+        {
+            return dalKhachHang.GetOnlyMaKH();
+        }
+
+    }
+}
---
+++ BLL_QuanLyNganHang/BUSTaiKhoan.cs
@@ -0,0 +1,90 @@
+﻿using DAL_QuanLyNganHang;
+using DTO_QuanLyNganHang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_QuanLyNganHang
+{
+    public class BUSTaiKhoan
+    {
+        DALTaiKhoan dalTaiKhoan = new DALTaiKhoan();
+        public List<TaiKhoan> GetTaiKhoanList()
+        {
+            return dalTaiKhoan.selectAll();
+        }
+        public string InsertTaiKhoan(TaiKhoan tk)
+        {
+            try
+            {
+                // Sinh mã số tài khoản mới
+                tk.SoTaiKhoan = dalTaiKhoan.generateSoTaiKhoan();
+
+                // Kiểm tra mã khách hàng có hợp lệ không (tùy bạn có DAL kiểm tra hay không)
+                if (string.IsNullOrEmpty(tk.MaKH))
+                {
+                    return "Mã khách hàng không hợp lệ.";
+                }
+
+                // Thêm vào CSDL
+                dalTaiKhoan.insertTaiKhoan(tk);
+
+                return string.Empty; // Thành công
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        public string UpdateTaiKhoan(TaiKhoan tk)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(tk.SoTaiKhoan))
+                {
+                    return "Số tài khoản không hợp lệ.";
+                }
+
+                dalTaiKhoan.updateTaiKhoan(tk);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Cập nhật không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+
+        public string DeleteTaiKhoan(string soTaiKhoan)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(soTaiKhoan))
+                {
+                    return "Số tài khoản không hợp lệ.";
+                }
+
+                dalTaiKhoan.deleteTaiKhoan(soTaiKhoan);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Xóa không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        public List<string> GetAllMaKH()
+        {
+            return dalTaiKhoan.GetOnlyMaKH();
+        }
+        public List<TaiKhoan> GetTaiKhoanTheoMaKH(string maKH)
+        {
+            string sql = "SELECT * FROM TaiKhoan WHERE MaKH = @0";
+            List<object> thamSo = new List<object> { maKH };
+            return dalTaiKhoan.SelectBySql(sql, thamSo);
+        }
+
+    }
+}
