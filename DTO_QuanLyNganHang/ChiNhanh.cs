40c3a2cb1b8a9198f569c989f032521261df0043
Colek123 <colek.2k@gmail.com>
2025-09-03 16:08:33 +0700

    done DTO ChiNhanh

---
+++ DTO_QuanLyNganHang/ChiNhanh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QuanLyNganHang
+{
+    public class Chi_nhánh
+    {
+        public string MaCN { get; set; }
+        public string TenCN { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
+    }
+}
