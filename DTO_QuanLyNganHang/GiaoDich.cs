52f0f4f93a2d081e098c87f11d4405b157a79eca
Colek123 <colek.2k@gmail.com>
2025-09-06 15:27:12 +0700

    done dto/GiaoDich

---
+++ DTO_QuanLyNganHang/GiaoDich.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QuanLyNganHang
+{
+    public class GiaoDich
+    {
+        public string MaGD { get; set; }
+        public string SoTaiKhoan { get; set; }  
+        public DateTime NgayGD { get; set; }
+        public decimal SoTien {  get; set; }
+        public string LoaiGD { get; set; }
+        public string MoTa { get; set; }
+        public bool TrangThai { get; set; }
+        public string TrangThaiText => TrangThai ? "Đã thanh toán" : "Đã hủy";
+    }
+}
